51bb9d5fff7490d9999cfaa41ac588bfb60ae711
Josh Bruce <josh@8fold.pro>
2024-02-02 15:58:28 -0500

    add: Calendly link

--- site-root/public/index.php
+++ site-root/public/index.php
@@ -9,18 +9,18 @@ require_once('./header.php');
 	<section>
 		<h2 id="time">Time</h2>
 		<p>Create or upgrade your time management system.</p>
-		<p><a href="https://calendly.com/joshbruce/lets-talk">Schedule<br>free consultation</a></p>
+		<p><a href="https://calendly.com/mastering-the-mundane/30min">Schedule<br>free consultation</a></p>
 		<p><a href="https://leanpub.com/master-the-mundane">Sign up for<br>book notifications</a></p>
 	</section>
 	<section>
 		<h2 id="space">Space</h2>
 		<p>Make your physical and digital spaces work <em>with</em> you.</p>
-		<p><a href="https://calendly.com/joshbruce/lets-talk">Schedule<br>free consultation</a></p>
+		<p><a href="https://calendly.com/mastering-the-mundane/30min">Schedule<br>free consultation</a></p>
 	</section>
 	<section>
 		<h2 id="money">Money</h2>
 		<p>Get a grip on your finances and relationship with money.</p>
-		<p><a href="https://calendly.com/joshbruce/lets-talk">Schedule<br>free consultation</a></p>
+		<p><a href="https://calendly.com/mastering-the-mundane/30min">Schedule<br>free consultation</a></p>
 	</section>
 </article>
 <?php require_once('./foot.php'); ?>
