b15751a3bed3b84b785d1f0b4d7d0a10ea97fc66
Josh Bruce <josh@8fold.pro>
2024-02-03 18:04:17 -0500

    fix: Constants can't be there, because used by $poster

--- site-root/public/index.php
+++ site-root/public/index.php
@@ -1,13 +1,14 @@
 <?php
 $relativeToRoot = './';
 
+require_once($relativeToRoot . 'constants.php');
+
 $pageTitle = "Mastering the Mundane";
 
 $description = 'Josh Bruce helps individuals make peace with time, space, and money. Whether itʼs physical, digital, or downsizing, Josh can probably help. Josh Bruce has been working as a coach for over a decade and is a self-proclaimed productivity super-freak.';
 
 $poster = $url . '/media/poster.png';
 
-require_once($relativeToRoot . 'constants.php');
 require_once($relativeToRoot . 'head.php');
 require_once($relativeToRoot . 'header.php');
 ?>
