37127d41745cd03c9e3df57a8a566455014a60ac
Josh Bruce <josh@8fold.pro>
2024-02-03 17:58:50 -0500

    add: Constants

---
+++ site-root/public/constants.php
@@ -0,0 +1,7 @@
+<?php
+$domain = 'mastering-the-mundane.com';
+
+$url = 'https://' . $domain;
+
+$keywords = 'Time Management, Self Management, Life Management, Life Coaching, Productivity Coaching, Financial Coaching, Professional Organization, bookkeeping, accoutability partner, digital organization, onsite, on-site, remote, hybrid, josh, bruce, josh bruce';
+?>
