a284ca0a98b1a6f93e1b2444c866c5fc9bc9997f
Josh Bruce <josh@8fold.pro>
2024-02-20 12:14:36 -0500

    extract: Calendly widget

--- site-root/public/booking/index.php
+++ site-root/public/booking/index.php
@@ -15,16 +15,7 @@ require_once($relativeToRoot . 'header.php');
 <article is="booking">
     <section>
         <h2>Book 30 minute free consultation</h2>
-
-        <noscript>
-            <p>Please follow this link to our <a href="https://calendly.com/mastering-the-mundane/30min?month=2024-02">scheduling provider</a> (Calendly) and be sure JavaScript is enabled for that site.</p>
-            <p><small>The booking widget requires JavaScript. If you donʼt want to enable JavaScript for our site, we understand.</small></p>
-        </noscript>
-        <p>The scheduling view may take a moment to appear.</p>
-        <!-- Calendly inline widget begin -->
-        <div class="calendly-inline-widget" data-url="https://calendly.com/mastering-the-mundane/30min?primary_color=831601" style="min-width:320px;height:700px;"></div>
-        <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
-        <!-- Calendly inline widget end -->
+        <?php require_once($relativeToRoot . 'calendly-widget.php'); ?>
     </section>
 </article>
 <?php require_once($relativeToRoot . 'foot.php'); ?>
