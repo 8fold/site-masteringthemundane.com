2b719e1700233a74d15e36664e7666628a1de89f
Josh Bruce <josh@8fold.pro>
2024-02-23 09:58:36 -0500

    BC: Convert booking to coaching and mentoring

---
+++ site-root/public/coaching-and-mentoring/index.php
@@ -0,0 +1,32 @@
+<?php
+$relativeToRoot = '../';
+
+require_once($relativeToRoot . 'constants.php');
+
+$pageTitle = "Coaching and Mentoring | Mastering the Mundane";
+
+$description = 'Josh Bruce helps individuals make peace with time, space, and money. Whether itʼs physical, digital, or downsizing, Josh can probably help. Josh Bruce has been working as a coach for over a decade and is a self-proclaimed productivity super-freak.';
+
+$poster = $url . '/media/poster.png';
+
+require_once($relativeToRoot . 'head.php');
+require_once($relativeToRoot . 'header.php');
+?>
+<article is="booking">
+    <section>
+        <h2>Coaching and Mentoring</h2>
+        <p>We will work together to update and refine your current systems. Weʼll focus primarily on your systems for time, space, and money. However, we may explore any subject youʼd like as long as I feel comfortable in my ability to help you navigate it. One of the explicit goals is to help you become independent of me over time as opposed to making you dependent upon me and any guidance I may be able to provide.</p>
+        <p><b>Before we decide to work together</b>, schedule <a href="https://cal.com/mastering-the-mundane/introduction" target="_blank">a 30-minute introduction</a> (no cost).</p>
+        <p>The introduction allows us to get to know one another and decide whether to embark on this journey together and how.</p>
+        <p>If weʼve had our introduction and decided to work with one another, youʼll either schedule <a href="https://cal.com/mastering-the-mundane/ad-hoc-session" target="_blank">an ad-hoc session</a> or <a href="https://cal.com/mastering-the-mundane/member-session" target="_blank">a member session</a>.</p>
+        <h3>How it works</h3>
+        <p>Sessions are 60 minutes in duration.</p>
+        <p><b>Ad-hoc sessions are 100 <abbr title="United States Dollars">USD</abbr> per session</b> and usually focus on a very specific point of friction in your life.</p>
+        <p><b>Membership is 50 USD per month</b> and allows you to schedule one 60-minute session per billing period. Additional sessions scheduled within a billing period are considered ad-hoc sessions, and billed separately.</p>
+        <p>For virtual and video calls I usually use <a href="https://www.gather.town" target="_blank">Gather</a>. You donʼt need to download or install anything, but you can. If we decide to use Gather, Iʼll send a link on the day of our scheduled session.</p>
+        <p>Phone and in-person are also options. If in-person requires travel, additional costs will be billed separately.</p>
+        <h3>Services</h3>
+        <p>We also offer more flat-fee services as well.</p>
+    </section>
+</article>
+<?php require_once($relativeToRoot . 'foot.php'); ?>
