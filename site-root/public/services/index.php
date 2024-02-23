fcafd843bc74ec9b86cc084395aba2f56a397feb
Josh Bruce <josh@8fold.pro>
2024-02-23 11:40:03 -0500

    add: Services page

---
+++ site-root/public/services/index.php
@@ -0,0 +1,49 @@
+<?php
+$relativeToRoot = '../';
+
+require_once($relativeToRoot . 'constants.php');
+
+$pageTitle = "Services | Mastering the Mundane";
+
+$description = 'Josh Bruce has spent his life trying to make peace with time, space, and money while also helping others do the same. Heʼs worked with individuals, The United States Federal Government Agencies, micro-businesses, Fortune 500 companies in the for-profit and not-for-profit categories.';
+
+$poster = $url . '/media/poster.png';
+
+require_once($relativeToRoot . 'head.php');
+require_once($relativeToRoot . 'header.php');
+?>
+<article is="services">
+    <section>
+        <h2>Services</h2>
+        <p>Unlike our <a href="/coaching-and-mentoring/">coaching and mentoring</a> our flat-fee services tend to be more about delegation and collaboration. In other words, I will be there directly performing or working with you to make progress on the things that are meaningful to you.</p>
+        <p><b>Before we decide to work together</b>, schedule <a href="https://cal.com/mastering-the-mundane/introduction" target="_blank">a 30-minute introduction</a> (no cost).</p>
+        <dl>
+            <dt>Bookkeeping</dt>
+            <dd>
+                <p>200 <abbr title="United States Dollars">USD</abbr> per month</p>
+                <ul>
+                    <li>Defining bookkeeping policies and procedures.</li>
+                    <li>Aid in creating investment policy statements.</li>
+                    <li>Reconciling accounts and categorizing transactions (General Ledger).</li>
+                    <li>Setting up accounts with your financial institution.</li>
+                    <li>Setting up accounting software.</li>
+                    <li>Accountability partner.</li>
+                </ul>
+                <p>After initial setup, I will reconcile and categorize your financial accounts and send you a report once a month. We can schedule a one-hour session per month to discuss how you are doing in working toward your financial goals and if things seem out of the ordinary.</p>
+            </dd>
+            <dt>Professional Organizing</dt>
+            <dd>
+                <p>800 USD for an 8-hour session (500 USD for a 4-hour session)</p>
+                <ul>
+                    <li>Defining acquisition (acquiring), maintenance, and disposition (removing) policies and procedures.</li>
+                    <li>Digital and physical space decluttering and organizing.</li>
+                    <li>Closet, studio apartment, mansion, cellphone, laptop, desktop, or cloud; we got this!</li>
+                </ul>
+                <p>Our time together is yours. If you want to take it slow, weʼll take it slow. If you want to get all the things done, we will do as much as we can in the time we have. We will usually work in 45 minute blocks followed by a 10-minute break. For the 8-hour session, there will be a one-hour lunch. If you tend to fatigue quickly from movement and decision making, we can spread the session across multiple days.</p>
+                <p>This is <em>not</em> a house cleaning or interior design service. Junk removal, product purchases, and travel may be charged separately.</p>
+            </dd>
+        </dl>
+        <p>Iʼm here to help, not add to any baggage you might have accumulated around time, space, and money. Therefore, there will be minimal judgement and maximum emphasis on health and safety.</p>
+    </section>
+</article>
+<?php require_once($relativeToRoot . 'foot.php'); ?>
