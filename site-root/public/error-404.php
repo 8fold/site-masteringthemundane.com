ae36ca3895db7425ffef10a996c3f5a5b068a3a1
Josh Bruce <josh@8fold.pro>
2024-02-23 11:38:42 -0500

    add: Error pages

---
+++ site-root/public/error-404.php
@@ -0,0 +1,22 @@
+<?php
+$relativeToRoot = './';
+
+require_once($relativeToRoot . 'constants.php');
+
+$pageTitle = "Mastering the Mundane";
+
+$description = 'Josh Bruce helps individuals make peace with time, space, and money. Whether itʼs physical, digital, or downsizing, Josh can probably help. Josh Bruce has been working as a coach for over a decade and is a self-proclaimed productivity super-freak.';
+
+$poster = $url . '/media/poster.png';
+
+require_once($relativeToRoot . 'head.php');
+require_once($relativeToRoot . 'header.php');
+?>
+<article>
+	<section>
+		<h2>Error: Page not found</h2>
+		<p>We could not locate the page you were trying to find.</p>
+        <p>Use the navigation to poke around the site.</p>
+	</section>
+</article>
+<?php require_once($relativeToRoot . 'foot.php'); ?>
---
+++ site-root/public/error-405.php
@@ -0,0 +1,21 @@
+<?php
+$relativeToRoot = './';
+
+require_once($relativeToRoot . 'constants.php');
+
+$pageTitle = "Mastering the Mundane";
+
+$description = 'Josh Bruce helps individuals make peace with time, space, and money. Whether itʼs physical, digital, or downsizing, Josh can probably help. Josh Bruce has been working as a coach for over a decade and is a self-proclaimed productivity super-freak.';
+
+$poster = $url . '/media/poster.png';
+
+require_once($relativeToRoot . 'head.php');
+require_once($relativeToRoot . 'header.php');
+?>
+<article>
+	<section>
+		<h2>Error: Unsupported method</h2>
+		<p>This site only allows the <abbr title="Hypertext Transfer Protocol">HTTP</abbr> GET and HEAD methods.</p>
+	</section>
+</article>
+<?php require_once($relativeToRoot . 'foot.php'); ?>
