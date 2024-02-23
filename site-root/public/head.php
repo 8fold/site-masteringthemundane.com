59161c5ddd3d6e89076c6e4d73ca000c72dd4fee
Josh Bruce <josh@8fold.pro>
2024-02-23 09:57:21 -0500

    update: CSS to make grid only on home page

--- site-root/public/head.php
+++ site-root/public/head.php
@@ -37,6 +37,6 @@
   	<meta name="twitter:image" content="<?php print($poster); ?>">
 
     <!-- Styles w/ query cache break -->
-    <link type="text/css" rel="stylesheet" href="/css/main.css?v=1.0.0">
+    <link type="text/css" rel="stylesheet" href="/css/main.css?v=1.0.1">
 </head>
 <body>
